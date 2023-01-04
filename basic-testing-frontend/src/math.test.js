b42b53e10ce774172fa6b37f31a687530f8d42f3
Edgar Klepek <edgarklepek@gmail.com>
2023-01-04 14:25:40 +0100

    Find and fix errors with the help of tests

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -12,3 +12,21 @@ it("should summarize all number values in an array", () => {
 	// Assert
 	expect(result).toBe(3);
 });
+
+it('should yield NaN if at least one invalid number is provided', () => {
+	const inputs = ["invalid", 1];
+
+	const result = add(inputs);
+
+	expect(result).toBeNaN();
+});
+
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+	const numbers = ["1", "2"];
+
+	const result = add(numbers);
+
+	const expectedResult = numbers.reduce((prevValue, curValue) => +prevValue + +curValue, 0);
+
+	expect(result).toBe(expectedResult);
+}); 
