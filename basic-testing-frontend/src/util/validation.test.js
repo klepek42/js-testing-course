af5a7b04d385b64291dec7c509648f30da5b3b0b
Edgar Klepek <edgarklepek@gmail.com>
2023-01-05 13:31:09 +0100

    Introduce test suites

--- basic-testing-frontend/src/util/validation.test.js
+++ basic-testing-frontend/src/util/validation.test.js
@@ -1,43 +1,49 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 import { validateStringNotEmpty, validateNumber } from "./validation";
 
-it('should throw error when string is empty', () => {
-	const input = "";
+describe("validateStringNotEmpty", () => {
+	it('should throw error when string is empty', () => {
+		const input = "";
 
-	const resultFn = () => {
-		validateStringNotEmpty(input);
-	}
+		const resultFn = () => {
+			validateStringNotEmpty(input);
+		}
 
-	expect(resultFn).toThrow(/Invalid input - must not be empty./);
+		expect(resultFn).toThrow(/Invalid input - must not be empty./);
+	});
+
+	it('should not throw an error when input is valid', () => {
+		const input = "Valid text";
+
+		const resultFn = () => {
+			validateStringNotEmpty(input)
+		}
+
+		expect(resultFn).not.toThrow("Invalid input - must not be empty.");
+	});
 });
 
-it('should not throw an error when input is valid', () => {
-	const input = "Valid text";
+describe("validateNumber", () => {
+	it('should throw error when number input is invalid', () => {
+		const input = "A text";
 
-	const resultFn = () => {
-		validateStringNotEmpty(input)
-	}
+		const resultFn = () => {
+			validateNumber(input);
+		}
 
-	expect(resultFn).not.toThrow("Invalid input - must not be empty.");
+		expect(resultFn).toThrow("Invalid number input.");
+	});
+
+	it('should not throw an error when number input is valid', () => {
+		const input = 1;
+
+		const resultFn = () => {
+			validateNumber(input);
+		}
+
+		expect(resultFn).not.toThrow("Invalid number input.");
+	});
 });
 
 
-it('should throw error when number input is invalid', () => {
-	const input = "A text";
 
-	const resultFn = () => {
-		validateNumber(input);
-	}
-
-	expect(resultFn).toThrow("Invalid number input.");
-});
-
-it('should not throw an error when number input is valid', () => {
-	const input = 1;
-
-	const resultFn = () => {
-		validateNumber(input);
-	}
-
-	expect(resultFn).not.toThrow("Invalid number input.");
-});
