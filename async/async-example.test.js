f401ed58c3cca20bb4a63d134aeda9642d94c173
Edgar Klepek <edgarklepek@gmail.com>
2023-01-06 17:20:22 +0100

    Add test with asynchronous code with a callback

---
+++ async/async-example.test.js
@@ -0,0 +1,21 @@
+import { it, expect } from "vitest";
+
+import { generateToken } from "./async-example";
+
+it('should generate a token value', (done) => {
+	const testUserEmail = "test@test.com";
+
+	generateToken(testUserEmail, (err, token) => {
+		// expect(token).toBeDefined();
+
+		try {
+			expect(token).toBeDefined();
+			// expect(token).toBe(2);
+			done();
+		}
+		catch (err) {
+			done(err);
+		}
+
+	});
+}); 
