2ffedac024291d3d2c9152de9a8d0ba4a2996a6b
Edgar Klepek <edgarklepek@gmail.com>
2023-02-17 14:30:30 +0100

    80. Using Local Mock Values

--- more-mocking/posts/posts.test.js
+++ more-mocking/posts/posts.test.js
@@ -1,4 +1,4 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, beforeEach } from "vitest";
 import { extractPostData } from "./posts";
 
 const testTitle = "Test title";
