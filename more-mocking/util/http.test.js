1f9d7686306eaf09c0104252d85569e0bcd9d414
Edgar Klepek <edgarklepek@gmail.com>
2023-02-17 14:17:17 +0100

    78. Test-specific Mocking Logic

--- more-mocking/util/http.test.js
+++ more-mocking/util/http.test.js
@@ -1,5 +1,6 @@
 import { describe, it, expect, vi } from "vitest";
 import { sendDataRequest } from "./http";
+import { HttpError } from "./errors";
 
 const testReponseData = {testKey: "testData"};
 
@@ -42,4 +43,28 @@ it('should convert the provided data to JSON before sending the request', async 
 	}
 
 	expect(errorMessage).not.toBe("Not a string!");
+});
+
+it('should throw an HttpError in case of non-okay responses', () => {
+	testFetch.mockImplementationOnce((url, options) => {
+		return new Promise((resolve, reject) => {
+
+			if (typeof options.body !== "string") {
+				reject("Not a string!");
+			}
+			const testResponse = {
+				ok: false,
+				json() {
+					return new Promise((resolve, reject) => {
+						resolve(testReponseData);
+					});
+				},
+			};
+			resolve(testResponse);
+		});
+	});
+
+	const testData = {key:"test"};
+
+	return expect(sendDataRequest(testData)).rejects.toBeInstanceOf(HttpError);
 });
