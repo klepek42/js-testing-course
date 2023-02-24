cd2c7b911e89b4f8966bbeb46a7a63ead8b363a3
Edgar Klepek <edgarklepek@gmail.com>
2023-02-24 11:42:51 +0100

    85. Setting up a Virtual HTML Page

--- the-dom/util/dom.test.js
+++ the-dom/util/dom.test.js
@@ -1,6 +1,18 @@
-import { it } from "vitest";
+import fs from "fs";
+import path from "path";
+
+import { it, vi } from "vitest";
+import { Window } from "happy-dom";
 
 import { showError } from "./dom";
+
+const htmlDocPath = path.join(process.cwd(), "index.html");
+const htmlDocumentContent = fs.readFileSync(htmlDocPath).toString();
+
+const window = new Window();
+const document = window.document;
+document.write(htmlDocumentContent);
+vi.stubGlobal("document", document);
 
 it("first test", () => {
 	showError("test");
